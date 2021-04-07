195326d8d75f2ef770afc7b5b79a335982bb4a30
raymondchuu <raymondchu1996@gmail.com>
2021-04-07 15:53:12 -0400

    updated readme

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 using System;
 using System.Windows.Forms;
 
+/**
+ * Name: Raymond Chu
+ * Scope: QBuild Challenge
+ * 
+ * This C# project is an application that generates visual representation of a Bill of Materials for a Valve part using a treeview and a datagridview to organize and display the data.
+ * 
+ * Logic: 
+ * 1. Generate connection to sql database
+ * 2. Button onclick that generates a tree of the materials using the static BOM database
+ * 3. Depending on the node selected from the treeview, perform a sql query to search for the child parts of the selected node, and fill datagridview with the required information
+ * 
+ */
+
 namespace qbuild_coding_challenge
 {
     static class Program
---
+++ README.md
@@ -0,0 +1,2 @@
+# QBuild Challenge
+This project was coded in C# and generates a treeview and datagridview to organize and display data from a SQL database.
