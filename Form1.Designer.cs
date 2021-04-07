d12e90f037fa824650a25919adfc627d1a7280e1
raymondchuu <raymondchu1996@gmail.com>
2021-04-07 15:46:37 -0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace qbuild_coding_challenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.popTreeDataBtn = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.partPathLabel = new System.Windows.Forms.Label();
+            this.currPartLabel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // popTreeDataBtn
+            // 
+            this.popTreeDataBtn.Location = new System.Drawing.Point(416, 151);
+            this.popTreeDataBtn.Name = "popTreeDataBtn";
+            this.popTreeDataBtn.Size = new System.Drawing.Size(142, 23);
+            this.popTreeDataBtn.TabIndex = 0;
+            this.popTreeDataBtn.Text = "Populate Data In Tree";
+            this.popTreeDataBtn.UseVisualStyleBackColor = true;
+            this.popTreeDataBtn.Click += new System.EventHandler(this.popTreeDataBtn_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(149, 77);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(233, 148);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(416, 194);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(142, 23);
+            this.exitBtn.TabIndex = 2;
+            this.exitBtn.Text = "Exit From Application";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titleLabel.Location = new System.Drawing.Point(149, 13);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(422, 33);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Testing Functionality for Tree and Datagrid";
+            // 
+            // partPathLabel
+            // 
+            this.partPathLabel.AutoSize = true;
+            this.partPathLabel.ForeColor = System.Drawing.Color.Red;
+            this.partPathLabel.Location = new System.Drawing.Point(416, 77);
+            this.partPathLabel.Name = "partPathLabel";
+            this.partPathLabel.Size = new System.Drawing.Size(99, 15);
+            this.partPathLabel.TabIndex = 4;
+            this.partPathLabel.Text = "Parent Child Part:";
+            // 
+            // currPartLabel
+            // 
+            this.currPartLabel.AutoSize = true;
+            this.currPartLabel.ForeColor = System.Drawing.Color.Green;
+            this.currPartLabel.Location = new System.Drawing.Point(416, 107);
+            this.currPartLabel.Name = "currPartLabel";
+            this.currPartLabel.Size = new System.Drawing.Size(74, 15);
+            this.currPartLabel.TabIndex = 5;
+            this.currPartLabel.Text = "Current Part:";
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Beige;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 267);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Bisque;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(639, 198);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 510);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.currPartLabel);
+            this.Controls.Add(this.partPathLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.popTreeDataBtn);
+            this.Name = "Form1";
+            this.Tag = "";
+            this.Text = "Testing Functionality for Tree and Datagrid";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button popTreeDataBtn;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label partPathLabel;
+        private System.Windows.Forms.Label currPartLabel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace qbuild_coding_challenge
+{
+    public partial class Form1 : Form
+    {
+        // global sql connection that remains open throughout the life of application
+        // csv files were imported using sql server
+        private SqlConnection conn = null;
+
+        // constructor to initialize this component
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // creates a new sql connection and opens it before the form is displayed for the first time to use for the datagridview
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(getConnectionString()); // makes connection with local database file
+                conn.Open();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.ExitThread(); // exits program if there is an error with connection
+            }
+        }
+
+        // returns the connection string so that this project can run anywhere as long as database file is within this project directory
+        static private string getConnectionString()
+        {
+            // current WORKING directory (i.e. \bin\debug)
+            string workingDirectory = Environment.CurrentDirectory;
+
+            // current PROJECT directory, which is what I need
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+
+            return "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + projectDirectory + "\\QbuildParts.mdf;Integrated Security=True;Connect Timeout=30;";
+        }
+
+        // onclick event handler to generate the treeview
+        private void popTreeDataBtn_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear(); // clears any previous nodes from treeview in case there were any
+
+            // makes a temporary local sql connection just to query the bom table to generate the tree
+            using (SqlConnection treeConn = new SqlConnection(getConnectionString()))
+            try
+            {
+                treeConn.Open();
+
+                using (SqlCommand cmd = new SqlCommand("SELECT COMPONENT_NAME, PARENT_NAME FROM BOM", treeConn))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    string rootName = findRootNode(); // returns the component name of the root node (VALVE)
+                    TreeNode root = new TreeNode(rootName); // create a treenode for the root node
+
+                    // hashmap to store the component name as a key, and list of treenodes as value to store nodes of duplicate components with different parents
+                    Dictionary<string, List<TreeNode>> parts = new Dictionary<string, List<TreeNode>>();
+                        parts.Add(rootName, new List<TreeNode>()); // add root's component name as the key and initialize treenode list as value
+                        parts[rootName].Add(root); // add the root treenode to the list
+
+                    while (reader.Read())
+                    {
+                        string compName = reader["COMPONENT_NAME"].ToString(); // component name
+                        string parentName = reader["PARENT_NAME"].ToString(); // parent name
+
+                        // dont need root value anymore
+                        if (!parentName.Equals(""))
+                        {
+                            // check if component exists in hashmap, and adds to hashmap if it's not in yet
+                            if (!parts.ContainsKey(compName))
+                            {
+                                parts.Add(compName, new List<TreeNode>());
+                            }
+
+                            // instantiate new treenode for the component and adds to the list
+                            parts[compName].Add(new TreeNode(compName));
+
+                            // searches hashmap for the parent node, and appends it as a child to the parent node
+                            // new nodes are pushed to the end of the list, so we always add the most recent node to append to the previous node
+                            parts[parentName].Last().Nodes.Add(parts[compName].Last());
+                        }
+                    }
+
+                    treeView1.Nodes.Add(root); // add root of generated tree to treeview
+
+                    // disables click button after the rows of query has been read
+                    if (reader.Read() == false)
+                    {
+                        popTreeDataBtn.Enabled = false;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.ExitThread();
+            }
+        }
+
+        // performs sql query to find the root node of the data and returns the component name of root node
+        private static string findRootNode()
+        {
+            using (SqlConnection rootConn = new SqlConnection(getConnectionString()))
+            {
+                rootConn.Open();
+
+                string root = "";
+
+                // root node will not have parent name
+                string rootQuery = "SELECT COMPONENT_NAME FROM BOM WHERE PARENT_NAME=''";
+
+                SqlCommand findRoot = new SqlCommand(rootQuery, rootConn);
+                SqlDataReader reader = findRoot.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    root = reader["COMPONENT_NAME"].ToString();
+                }
+
+                reader.Close();
+
+                return root;
+            }
+
+        }
+
+        // event that triggers after selecting node on treeview, and updates the datagridview
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            // sql query string to search for the child components of the node being selected on the tree
+            string sqlSearchParts = "SELECT BOM.PARENT_NAME, BOM.COMPONENT_NAME, PART.PART_NUMBER, PART.TITLE, BOM.QUANTITY, PART.TYPE, PART.ITEM, PART.MATERIAL FROM BOM LEFT JOIN PART ON BOM.COMPONENT_NAME = PART.NAME WHERE BOM.PARENT_NAME = @parentName";
+
+            partPathLabel.Text = "Parent Child Part: " + treeView1.SelectedNode.FullPath; // updates label to view the parent/child of the part
+            currPartLabel.Text = "Current part: " + treeView1.SelectedNode.Text; // updates the label to view the current part selected
+
+            // uses the connection that was opened when the form loaded so that a new connection isn't being used upon every click of a new node
+            using (SqlDataAdapter adapter = new SqlDataAdapter(sqlSearchParts, conn))
+            {
+                // generate datagridview
+                adapter.SelectCommand.Parameters.AddWithValue("@parentName", treeView1.SelectedNode.Text); // prevents sql injection
+                DataTable dt = new DataTable();
+                adapter.Fill(dt); // fills datatable with the sql query result
+                dataGridView1.DataSource = dt; // updates datagridview with the datatable
+            }
+        }
+
+        // button onclick handler that closes the sql connection, and then closes the application
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            conn.Close();
+            Application.ExitThread();
+        }
+    }
+}
